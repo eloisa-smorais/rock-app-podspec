d782952f75dd25dca38647ee24a78b63a70b7f6d
Adabriand Furtado <adabriand.furtado@wefit.com.br>
2025-10-24 19:00:38 -0300

    feat: test integration

---
+++ Frameworks/HelloBrownfieldReact.xcframework/ios-arm64_x86_64-simulator/HelloBrownfieldReact.framework/Headers/HelloBrownfieldReact.h
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for HelloBrownfieldReact.
+FOUNDATION_EXPORT double HelloBrownfieldReactVersionNumber;
+
+//! Project version string for HelloBrownfieldReact.
+FOUNDATION_EXPORT const unsigned char HelloBrownfieldReactVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelloBrownfieldReact/PublicHeader.h>
